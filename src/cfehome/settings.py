6842d15ef7b1e292589690a46d17afb989f69ccf
Leoka2000 <reusoliveiraleo@gmail.com>
2024-08-28 19:49:06 +0200

    db

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'fundhancedb',
+    'NAME': 'fundhancedb2',
     'USER': 'fundhancedb_owner',
     'PASSWORD': 'RdirsP5hN2kc',
     'HOST': 'ep-round-bird-a2n4c50k.eu-central-1.aws.neon.tech',
